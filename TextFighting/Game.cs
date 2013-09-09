43e1d64ce5255397a3fc617ba27161d465adc26c
Marvis-Lin <t824675951535@hotmail.com>
2013-09-09 17:50:06 +0800

    te

--- TextFighting/Game.cs
+++ TextFighting/Game.cs
@@ -46,8 +46,6 @@ namespace TextFighting
             p2.lifePoint = 1;
             int ashlin = 1112;
             int marvis = 1;
-            p2.lifePoint = 10;
-            p2.lifePoint = 999;
 
         }
 
